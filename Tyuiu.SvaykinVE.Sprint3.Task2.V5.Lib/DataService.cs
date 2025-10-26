8cf5bbd10829f053678809b1079450e766f0f6f8
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-10-26 22:53:49 +0500

    Task2

---
+++ Tyuiu.SvaykinVE.Sprint3.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SvaykinVE.Sprint3.Task2.V5.Lib
+{
+    public class DataService : ISprint3Task2V5
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double res = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                res += (Math.Pow(value, i) + 1) * Math.Cos(i);
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SvaykinVE.Sprint3.Task2.V5/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.SvaykinVE.Sprint3.Task2.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Свайкин В. Е. | АСОиУб-25-1";
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* Спринт #3                                                                         *");
+        Console.WriteLine("* Тема: Оператор цикла do-while                                                     *");
+        Console.WriteLine("* Задание #2                                                                        *");
+        Console.WriteLine("* Вариант #5                                                                        *");
+        Console.WriteLine("* Выполнил: Свайкин Владислав Евгеньевич | АСОиУб-25-1                              *");
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+        Console.WriteLine("* Написать программу используя цикл do-while,которая вычисляет сумму ряда по формуле*");
+        Console.WriteLine("* при а = 0,75                                                                      *");
+        Console.WriteLine("*                                                                                   *");
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+        Console.WriteLine("*************************************************************************************");
+
+        double value = 0.75;
+        int startValue = 1;
+        int stopValue = 20;
+        double res = ds.GetSumSeries(value, startValue, stopValue);
+        Console.WriteLine("Переменная A = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+        Console.WriteLine("*************************************************************************************");
+
+        Console.WriteLine("Сумма ряда = " + res);
+        Console.ReadKey();
+    }
+}
